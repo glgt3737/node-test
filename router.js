2f0b1900b0e6975c80fc8209b9b3e1ef3d2cbda9
葛婷 <geting@chachazhan.com>
2016-08-26 10:29:17 +0800

    test

--- router.js
+++ router.js
@@ -5,7 +5,7 @@ const loginFilter = require('./utils/loginFilter');
 module.exports = (app) => {
 
   router.get('/users', require('./service/user/queryAllUsers'));
-  noLoginRouter.post('/users', require('./service/user/addUser'));
+  router.post('/users', require('./service/user/addUser'));
   router.patch('/users/:id', require('./service/user/updateUser'));
   router.delete('/users/:id', require('./service/user/deleteUser'));
   

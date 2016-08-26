fe027276f054c194d062740ca5ef11be386437dc
葛婷 <geting@chachazhan.com>
2016-08-26 10:22:23 +0800

    test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,23 @@ var pg = require('pg');
 var Sequelize = require('sequelize');
 
 module.exports = (req, res) => {
-  var sequelize = new Sequelize(process.env.DATABASE_URL);
+  var sequelize = new Sequelize(process.env.DATABASE_URL, {
+    pool: {
+      max: 10,
+      min: 0,
+      idle: 10000
+    },
+    timezone: '+08:00',
+    define: {
+      timestamps: true,
+      createdAt: 'created_time',
+      updatedAt: 'updated_time',
+      deletedAt: 'deleted_time',
+      paranoid: true,
+      underscored: true,    //默认false, true会自动将驼峰命名改为下划线
+      freezeTableName: true //默认false，修改表名为复数，true不修改表名
+    }
+  });
   sequelize.authenticate()
     .then(err => res.send('Connection has been established successfully'))
     .catch(err => res.send('Unable to connect to the database:', err));

aa7c3e3778ca41faca696cf105d967b62c156a9e
葛婷 <geting@chachazhan.com>
2016-08-26 10:13:46 +0800

    sql-test

--- test/test.js
+++ test/test.js
@@ -2,12 +2,14 @@ var pg = require('pg');
 
 module.exports = (req, res) => {
   pg.defaults.ssl = true;
-  pg.connect(process.env.DATABASE_URL, (err, client) => {
-    if(err) throw err;
-    console.log('Connected to postgres! Getting schemas...');
-
-    client
-      .query('select table_schema, table_name from information_schema.tables')
-      .on('row', row => res.send(JSON.stringify(row)));
+  pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+    if(err) res.send(err);
+    else {
+      client.query('select table_schema, table_name from information_schema.tables', (err, result) => {
+        done();
+        if(err) res.send(err);
+        else res.send(result);
+      })
+    }
   });
 };
